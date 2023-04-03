7a8c8e24ffea22fe68ea83f5203e3cea848f0920
canknbr <cankanbur1@gmail.com>
2023-04-03 13:21:27 +0300

    Initial Commit

---
+++ Hue Rotation Sample/Hue_Rotation_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hue_Rotation_SampleApp.swift
+//  Hue Rotation Sample
+//
+//  Created by Can Kanbur on 3.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Hue_Rotation_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
